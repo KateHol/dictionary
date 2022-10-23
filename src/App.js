57cd33fdb6355936506190ca8f92b622f5471a74
KateHol <golotakaterina@gmail.com>
2022-10-23 23:48:02 +0300

    add header

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ export default function App() {
   return (
     <div className="App">
       <div className='container'>
+        <header className='App-header'></header>
 <main>
 <Dictionary />
   </main>    
